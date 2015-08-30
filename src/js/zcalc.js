dc70019a05ffe5afc1c2b5bfe4dd44586801ca08
Alexander Khramushin <alkh34@yandex.ru>
2015-08-30 19:24:36 +0300

    автофокус на поле ввода, при нажатии на ENTER происходит расчет

--- src/js/zcalc.js
+++ src/js/zcalc.js
@@ -1,88 +1,103 @@
-var input = document.getElementById("user-input"),
-    calcButton = document.getElementById("calc-btn"),
-    resultElem = document.getElementById("result");
+window.onload = function () {
+    var input = document.getElementById("user-input"),
+        calcButton = document.getElementById("calc-btn"),
+        resultElem = document.getElementById("result");
 
-calcButton.onclick = handleClick;
+    // CONSTANTS
+    var ENTER_KEY = 13;
 
-// проверка ввода
-function checkInput(value) {
-    return (/^[\d]+$/).test(value);
-}
+    // перевести фокус на поле ввода
+    input.focus();
 
-// очистка ввода
-function cls() {
-    input.value = "";
-}
-
-// обработка клика
-function handleClick() {
-    var num = input.value;
+    calcButton.onclick = handleClick;
+    document.onkeydown = function (e) {
+        if (e.which == 13) {
+            handleClick();
+        }
+    };
 
     // проверка ввода
-    if (!checkInput(num)) {
-        cls();
-        printError(new Error("ошибка ввода"));
-        return;
+    function checkInput(value) {
+        return (/^[\d]+$/).test(value);
     }
 
-    var result = calculate(num);
-    cls();
-    printResult(result);
-}
+    // очистка ввода
+    function cls() {
+        input.value = "";
+    }
+
+    // обработка клика
+    function handleClick() {
+        var num = input.value;
+
+        // проверка ввода
+        if (!checkInput(num)) {
+            cls();
+            printError(new Error("ошибка ввода"));
+            input.focus();
+            return;
+        }
+
+        var result = calculate(num);
+        cls();
+        printResult(result);
+        input.focus();
+    }
 
 // вывод ошибки
-function printError(err) {
-    resultElem.innerHTML = '<span class="_error">' + err.message + '</span>';
-}
+    function printError(err) {
+        resultElem.innerHTML = '<span class="_error">' + err.message + '</span>';
+    }
 
 // вывод результата
-(function (){
-    this.printResult = function (result) {
-        if (typeof result === "undefined" && result.length == 0) {
-            return;
-        }
+    (function (){
+        this.printResult = function (result) {
+            if (typeof result === "undefined" && result.length == 0) {
+                return;
+            }
 
-        var max = result.length;
+            var max = result.length;
 
-        if (max == 1) {
-            resultElem.innerHTML = result[0];
-            return;
-        }
+            if (max == 1) {
+                resultElem.innerHTML = result[0];
+                return;
+            }
 
-        if (max == 2) {
-            resultElem.innerHTML = result[1];
-            return;
-        }
+            if (max == 2) {
+                resultElem.innerHTML = result[1];
+                return;
+            }
 
-        resultElem.innerHTML = '<span class="_small">' +
+            resultElem.innerHTML = '<span class="_small">' +
             result[max-2] +
             '</span><span class="_big">' +
             result[max-1] + "</span>";
-    };
-})();
+        };
+    })();
 
 // необходимые вычисления
 // возвращает массив результатов
 // первый элемент массива это исходноечисло
-function calculate(num) {
+    function calculate(num) {
 
-    var tempNum = 0,
-        results = [],
-        iterationLimit = 100,
-        currentIteration = 0;
-    results.push(num);
+        var tempNum = 0,
+            results = [],
+            iterationLimit = 100,
+            currentIteration = 0;
+        results.push(num);
 
-    while (num > 9 && currentIteration <= iterationLimit) {
-        num = num + "";
-        for (var n = 0; n < num.length; n++) {
-            tempNum = tempNum + parseInt(num.charAt(n));
+        while (num > 9 && currentIteration <= iterationLimit) {
+            num = num + "";
+            for (var n = 0; n < num.length; n++) {
+                tempNum = tempNum + parseInt(num.charAt(n));
+            }
+
+            num = tempNum;
+            results.push(num);
+            tempNum = 0;
+            currentIteration++;
         }
 
-        num = tempNum;
-        results.push(num);
-        tempNum = 0;
-        currentIteration++;
+        return results;
     }
-
-    return results;
-}
+};
